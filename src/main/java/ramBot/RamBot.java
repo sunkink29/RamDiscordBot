8e561f2626d9650889b4758037b8a36363828306
caleb parks <rps@calebs-mac.local>
2017-05-14 00:38:53 -0500

    changed the command system to be simpler to make new commands

--- src/main/java/ramBot/RamBot.java
+++ src/main/java/ramBot/RamBot.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.function.Predicate;
 
 import sx.blah.discord.api.*;
 import sx.blah.discord.api.events.*;
@@ -19,9 +18,7 @@ public class RamBot extends BaseBot implements IListener<MessageReceivedEvent> {
 	public IUser botManager;
 	public IChannel sunkink29Dm;
 	List<String> admins = new ArrayList<String>();
-	private List<String> actionNames = new ArrayList<String>(); 
-	private List<Predicate<Message>> actionConditions = new ArrayList<Predicate<Message>>();
-	private List<Consumer<Message>> actionList = new ArrayList<>();
+	private List<Command> commands = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		BaseBot.main(args);
@@ -35,11 +32,9 @@ public class RamBot extends BaseBot implements IListener<MessageReceivedEvent> {
 		admins.add("194936758696148992");
 		admins.add("285607196245622785");
 		
-		addAction("help", message -> message.content.contains("!help"), 
-				message -> actionNames.forEach(name -> sendMessage(message.message.getChannel(), name)));
+		addCommand("help", message -> commands.forEach(command -> sendMessage(message.message.getChannel(), command.name)));
 		
-		addAction("sendMessage", message -> message.content.contains("!sendMessage"),
-				message -> {
+		addCommand("sendMessage", message -> {
 					List<String> words = new ArrayList<String>(Arrays.asList(message.content.split("\\s+")));
 					List<IChannel> channels = new ArrayList<>();
 					List<String> guilds = new ArrayList<>();
@@ -47,12 +42,8 @@ public class RamBot extends BaseBot implements IListener<MessageReceivedEvent> {
 					words.stream().filter(word -> (word.contains("<") || (word.split("\\.").length > 1 && guilds.contains(word.split("\\.")[0]))) && 
 							!word.contains(client.getOurUser().getID()))
 					.forEach(channel ->{ 
-						if (channel.contains("<")){
-							try {
-								channels.add(client.getOrCreatePMChannel(client.getUserByID(channel.substring(2, channel.length()-1))));
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
+						if (channel.contains("<")) {
+							channels.add(getPMChannel(client.getUserByID(channel.substring(2, channel.length()-1))));
 						} else {
 							client.getGuilds().stream().filter(nChannel -> nChannel.getID().equals(channel.split("\\.")[0]))
 								.forEach(nChannel -> channels.add(nChannel.getChannelByID(channel.split("\\.")[1])));
@@ -63,29 +54,25 @@ public class RamBot extends BaseBot implements IListener<MessageReceivedEvent> {
 					for(IChannel channel: channels) { sendMessage(channel, sMessage);}
 				});
 		
-		addAction("addAdmin", message -> message.content.contains("!addAdmin"),
-				message -> message.message.getMentions().stream()
+		addCommand("addAdmin", message -> message.message.getMentions().stream()
 				.filter(mentionClient -> mentionClient != client.getOurUser() && !admins.contains(mentionClient.getID()))
 				.forEach(user -> admins.add(user.getID())));
 		
-		addAction("printAdmins", message -> message.content.contains("!printAdmins"),
-				message -> admins.forEach(admin -> sendMessage(message.message.getChannel(), client.getUserByID(admin).getName())));
+		addCommand("printAdmins", message -> admins.forEach(admin ->
+				sendMessage(message.message.getChannel(), client.getUserByID(admin).getName())));
 		
-		addAction("getGuild", message -> message.content.contains("!getGuild"),
-				message -> client.getGuilds().forEach(guild -> sendMessage(message.message.getChannel(), guild.getName()+" "+guild.getID())));
+		addCommand("getGuild", message -> client.getGuilds()
+				.forEach(guild -> sendMessage(message.message.getChannel(), guild.getName()+" "+guild.getID())));
 		
-		addAction("getUsers", message -> message.content.contains("!getUsers")
-				, message -> client.getUsers().forEach(user -> sendMessage(message.message.getChannel(), user.getName())));
+		addCommand("getUsers", message -> client.getUsers()
+				.forEach(user -> sendMessage(message.message.getChannel(), user.getName())));
 		
-		addAction("logout", message -> message.content.contains("!logout"),
-				message -> {sendMessage(message.message.getChannel(), "logging out");logout();});
-		addAction("roll", message -> message.content.contains("!roll"),
-				message -> {
-					List<String> words = new ArrayList<String>(Arrays.asList(message.content.split("\\s+")));
-					int commandIndex = 0;
-					for (String word: words) {if (word.equals("!roll")){commandIndex = words.indexOf(word);break;}}
-					int numberOfDice = Integer.parseInt(words.get(commandIndex+1));
-					int numberOfSides = Integer.parseInt(words.get(commandIndex+2).substring(1));
+		addCommand("logout", message -> {sendMessage(message.message.getChannel(), "logging out");logout();});
+		
+		addCommand("roll", message -> {
+					String[] words = message.args;
+					int numberOfDice = Integer.parseInt(words[0]);
+					int numberOfSides = Integer.parseInt(words[1].substring(1));
 					List<Integer> diceRolls = new ArrayList<>();
 					for (int i=0;i<numberOfDice;i++) diceRolls.add((int)(Math.random()*numberOfSides+1));
 					String output = "I rolled a ";
@@ -95,12 +82,10 @@ public class RamBot extends BaseBot implements IListener<MessageReceivedEvent> {
 					output += (diceRolls.size()>1?"for a total of "+total:"");
 					sendMessage(message.message.getChannel(), output);
 				;});
-		addAction("flip", message -> message.content.contains("!flip"),
-				message -> {
-					List<String> words = new ArrayList<String>(Arrays.asList(message.content.split("\\s+")));
-					int commandIndex = 0;
-					for (String word: words) {if (word.equals("!flip")){commandIndex = words.indexOf(word);break;}}
-					int numberOfcoins = Integer.parseInt(words.get(commandIndex+1));
+		
+		addCommand("flip", message -> {
+					String[] words = message.args;
+					int numberOfcoins = Integer.parseInt(words[0]);
 					List<Boolean> coinFlips = new ArrayList<>();
 					for (int i=0;i<numberOfcoins;i++) coinFlips.add((int)(Math.random()*2+1)==1);
 					int total=0;
@@ -123,9 +108,9 @@ public class RamBot extends BaseBot implements IListener<MessageReceivedEvent> {
 			System.out.println(message.getAuthor().getName()+ " : " + message.getContent());
 			boolean commandTriggered = false;
 			if (admins.contains(message.getAuthor().getID()) && message.getContent().contains("!")) {
-				for(int i = 0; i < actionConditions.size(); i++) {
-					if (actionConditions.get(i).test(new Message(message))) {
-						actionList.get(i).accept(new Message(message));
+				for(int i = 0; i < commands.size(); i++) {
+					if (commands.get(i).checkCondition(new Message(message))) {
+						commands.get(i).execute(new Message(message));
 						commandTriggered = true;
 						break;
 					}
@@ -143,9 +128,9 @@ public class RamBot extends BaseBot implements IListener<MessageReceivedEvent> {
 				content = content.replace("<@"+client.getOurUser().getID()+">", "");
 				output = aiClient.getResponce(content);
 				if (output.contains("!")) {
-					for(int i = 0; i < actionConditions.size(); i++) {
-						if (actionConditions.get(i).test(new Message(message, output))) {
-							actionList.get(i).accept(new Message(message, output));
+					for(int i = 0; i < commands.size(); i++) {
+						if (commands.get(i).checkCondition(new Message(message,output))) {
+							commands.get(i).execute(new Message(message,output));
 							commandTriggered = true;
 							break;
 						}
@@ -159,11 +144,9 @@ public class RamBot extends BaseBot implements IListener<MessageReceivedEvent> {
 		}
 	}
 	
-	public void addAction(String name, Predicate<Message> condition, Consumer<Message> action) {
-		if (!actionNames.contains(name)) {
-			actionNames.add(name);
-			actionConditions.add(condition);
-			actionList.add(action);
+	public void addCommand(String name, Consumer<Message> action) {
+		if (commands.stream().filter(command -> command.name == name).count() == 0) {
+			commands.add(new Command(name, action));
 		}
 	}
 	
@@ -196,6 +179,15 @@ public class RamBot extends BaseBot implements IListener<MessageReceivedEvent> {
 		}
 	}
 	
+	private IChannel getPMChannel(IUser user){
+		try {
+			return client.getOrCreatePMChannel(user);
+		} catch (DiscordException | RateLimitException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
 	private void logout() {
 		try {
 			client.logout();
@@ -203,11 +195,14 @@ public class RamBot extends BaseBot implements IListener<MessageReceivedEvent> {
 			e.printStackTrace();
 		}
 	}
+	
+	private void getArgsFromCall(){}
 }
 
 class Message {
-	public IMessage message;
-	public String content;
+	public final IMessage message;
+	public final String content;
+	public String[] args = new String[1];
 	
 	public Message (IMessage message) {
 		this(message, message.getContent());
@@ -216,5 +211,31 @@ class Message {
 	public Message (IMessage message, String content) {
 		this.message = message;
 		this.content = content;
+	}
+}
+
+class Command{
+	private final Consumer<Message> action;
+	public final String name;
+	
+	public Command(String name, Consumer<Message> action) {
+		this.action = action;
+		this.name = name;
+	}
+	
+	public boolean checkCondition(Message message){
+		return message.content.contains("!"+name);
+	}
+	
+	public void execute(Message message){
+		List<String> words = new ArrayList<String>(Arrays.asList(message.content.split("\\s+")));
+		if (words.size() > 0) {
+			while (words.get(0).equals("!"+name)){
+				System.out.println(words.get(0));
+				words.remove(0);
+			}
+		}
+		message.args = words.toArray(message.args);
+		action.accept(message);
 	}
 }
